a3c6fae63ad8a4ea992480dd057833035b73560f
Amirul Islam <AmirulAndalibGit@gmail.com>
2022-10-15 19:34:37 +0600

    Update challenge.py

--- src/apis/scaffold/challenge.py
+++ src/apis/scaffold/challenge.py
@@ -15,10 +15,10 @@ bricklayer = GameClaimer(email=config.epic_email, password=config.epic_password)
 
 
 def run(silence: typing.Optional[bool] = None):
-    """刷新上下文身份令牌"""
-    logger.info("STARTUP [ScaffoldChallenge] 正在更新身份令牌")
+    """Refreshing identity tokens"""
+    logger.info("STARTUP [ScaffoldChallenge] Updating identity token")
     if bricklayer.cookie_manager.refresh_ctx_cookies(silence=silence):
         ctx_cookies = bricklayer.cookie_manager.load_ctx_cookies()
         with open(PATH_USR_COOKIES, "w", encoding="utf8") as file:
             file.write(ToolBox.transfer_cookies(ctx_cookies))
-    logger.success(f"GET [ChallengeRunner] 任务结束 - path={PATH_USR_COOKIES}")
+    logger.success(f"GET [ChallengeRunner] Mission Accomplished!! - path={PATH_USR_COOKIES}")
