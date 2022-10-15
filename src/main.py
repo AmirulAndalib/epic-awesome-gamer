b36a6bf72fe4fb6162664559239760d0844daa09
Amirul Islam <AmirulAndalibGit@gmail.com>
2022-10-15 19:22:54 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 # Time       : 2022/1/16 0:24
 # Author     : QIN2DIM
 # Github     : https://github.com/QIN2DIM
-# Description: 🚀 哟！Epic免费人！
+# Description: 🚀 Yo Epic Free People!
 from fire import Fire
 
 from services.scaffold import Scaffold
